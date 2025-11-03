31a5167ce8464d81bb6eaaec5924c51e34610ee8
kotyash_misha <s0596636@htw-berlin.de>
2025-11-03 10:08:40 +0100

    M3 fertig

--- src/main/java/com/example/demo/HomEatEntryController.java
+++ src/main/java/com/example/demo/HomEatEntryController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class HomEatEntryController {
 
