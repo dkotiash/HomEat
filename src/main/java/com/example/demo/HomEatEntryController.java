4bdb1e944f8054fab7c4767a13264a2311d0c63d
dkotiash <s0596635@htw-berlin.de>
2025-11-04 20:29:45 +0100

    CrossOrigin with frontend

--- src/main/java/com/example/demo/HomEatEntryController.java
+++ src/main/java/com/example/demo/HomEatEntryController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "https://frontendvue-homeat.onrender.com"
+})
 @RestController
 public class HomEatEntryController {
 
